54c67788d6f8e3f9d7160a1eef446a46bfbb9d35
Juan J. Lorenzo Sáez <juan-lorenzo@hotmail.es>
2023-12-01 13:26:25 +0100

    first commit

---
+++ src/main/java/com/example/CaSBackend/CaSBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.CaSBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaSBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaSBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CaSBackend/CaSBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CaSBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaSBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
